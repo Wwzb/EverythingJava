da4e1a552bc818ad6becdb61dacaea1132415cde
Wwzb <996627951@qq.com>
2018-07-26 20:09:08 +0800

    加入响应功能

--- HttpServer/src/com/wzb/httpserver/Server.java
+++ HttpServer/src/com/wzb/httpserver/Server.java
@@ -1,16 +1,16 @@
 package com.wzb.httpserver;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 /**
  * 创建服务器并启动
  */
 public class Server {
+    public static final String CRLF="\r\n";
+    public static final String BLANK=" ";
     private ServerSocket server;
     public static void main(String[] args) {
         Server server = new Server();
@@ -43,6 +43,28 @@ public class Server {
             //接收客户端的请求信息
             String requestInfo = new String(data,0,len).trim();
             System.out.println(requestInfo);
+
+            //响应
+            StringBuilder responseContext = new StringBuilder();
+            responseContext.append("<html><head><title>这是一个响应</title>"+"</head><body>Hello WZB!</body></html>");
+            StringBuilder response = new StringBuilder();
+            //1)HTTP 协议版本、状态代码、描述
+            response.append("HTTP/1.1").append(BLANK).append("200").append(BLANK).append("OK").append(CRLF);
+            //2)响应头(response head)
+            response.append("Server:WZB Server/0.0.1").append(CRLF);
+            response.append("Date:").append(new Date()).append(CRLF);
+            response.append("Content-type:text/html;charset=utf-8").append(CRLF);
+            //正文长度
+            response.append("Content-Length:").append(responseContext.toString().getBytes().length).append(CRLF);
+            //3)正文
+            response.append(CRLF);
+            response.append(responseContext);
+
+            //输出流
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            bw.write(response.toString());
+            bw.flush();
+            bw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
